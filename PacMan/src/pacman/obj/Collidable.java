deba240a87d096a9e3d6f2052d13efd5cdf46296
KaMiKaZe6424 <kamikaze6424.doggames@gmail.com>
2015-01-11 12:40:10 +0100

    First commit to my JPacman project

---
+++ PacMan/src/pacman/obj/Collidable.java
@@ -0,0 +1,11 @@
+package pacman.obj;
+
+import javax.swing.JPanel;
+
+public abstract class Collidable extends JPanel {
+	
+	private static final long serialVersionUID = -652719543772601926L;
+
+	public abstract void update();
+	
+}
---
+++ PacMan/src/pacman/obj/PacMan.java
@@ -0,0 +1,12 @@
+package pacman.obj;
+
+public class PacMan extends Collidable {
+	
+	private static final long serialVersionUID = 4342964723168355959L;
+	
+	@Override
+	public void update() {
+		
+	}
+	
+}
